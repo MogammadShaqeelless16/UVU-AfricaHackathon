9e8e1175e0a8777577e4bcb00a891dd8a0a0b4f8
Darkinsator <jasonblankenberg02@gmail.com>
2024-05-16 23:48:56 +0200

    added swipe card

--- views/HomeScreen.js
+++ views/HomeScreen.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 import axios from 'axios';
+import SwipeCards from 'react-native-swipe-cards';
 import Navbar from '../components/Navbar';
 
 const HomeScreen = () => {
   const [posts, setPosts] = useState([]);
+  const [currentPostIndex, setCurrentPostIndex] = useState(0);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -19,22 +21,40 @@ const HomeScreen = () => {
     fetchPosts();
   }, []);
 
+  const handleYup = (post) => {
+    // Handle "yes" swipe action
+    console.log("Yes:", post.title.rendered);
+    // Move to the next post
+    setCurrentPostIndex(currentPostIndex + 1);
+  };
+
+  const handleNope = (post) => {
+    // Handle "no" swipe action
+    console.log("No:", post.title.rendered);
+    // Move to the next post
+    setCurrentPostIndex(currentPostIndex + 1);
+  };
+
   return (
     <View style={styles.container}>
       <Navbar navigation={navigation} />
-      <ScrollView contentContainerStyle={styles.content}>
-        <Text style={styles.text}>WordPress Posts</Text>
-        {posts.map(post => (
+      <SwipeCards
+        cards={posts}
+        renderCard={(post) => (
           <TouchableOpacity
-            key={post.id}
             style={styles.card}
             onPress={() => navigation.navigate('PostDetails', { postId: post.id })}
           >
             <Text style={styles.title}>{post.title.rendered}</Text>
             {/* Add more content from the post if needed */}
           </TouchableOpacity>
-        ))}
-      </ScrollView>
+        )}
+        handleYup={handleYup}
+        handleNope={handleNope}
+        cardRemoved={() => {}}
+        stack={false}
+        loop={false}
+      />
     </View>
   );
 };
@@ -44,22 +64,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#0fb5ce',
   },
-  content: {
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 20,
-    marginBottom: 20,
-    marginTop: 20,
-    fontWeight: 'bold',
-    color: '#fff',
-  },
   card: {
     backgroundColor: '#fff',
     padding: 20,
     borderRadius: 10,
     marginBottom: 20,
     width: '90%',
+    alignItems: 'center',
   },
   title: {
     fontSize: 18,
