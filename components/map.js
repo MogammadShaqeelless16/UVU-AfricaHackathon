e4526cb1909708f0dcc05c9de55a4774e55c03cb
Shaqeel Less <54390756+MogammadShaqeelless16@users.noreply.github.com>
2024-05-17 13:36:31 +0000

    Update

--- components/map.js
+++ components/map.js
@@ -36,7 +36,6 @@ const Map = () => {
         >
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           />
           <LeafletMarker position={[userLocation.latitude, userLocation.longitude]}>
             <Popup>
